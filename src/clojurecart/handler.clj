ab5c254faa955ff0d0419930808319492b726418
silvia <silvia.schreier@innoq.com>
2013-03-19 15:24:16 +0100

    refactoring

--- src/clojurecart/handler.clj
+++ src/clojurecart/handler.clj
@@ -4,37 +4,35 @@
             [compojure.route :as route]
             [com.twinql.clojure.conneg :as conneg]))
 
-(defn render-response [resource headers method]
+(defn render-response [resource headers]
   (if (nil? resource)
     {:status 404}
     (let [format (conneg/best-allowed-content-type 
                    (get headers "accept") 
                    (-> resource
-                     (get :get)
                      (get :produced)))]
       (if (nil? format)
         {:status 406}
         (-> resource
-          (get method)
           (get :response)
           (get format))))))
 
 (defroutes app-routes
   (GET "/" 
        {:keys [headers request-method]} 
-       (render-response (root) headers request-method))
+       (render-response (request-method (root)) headers))
   (GET users-route 
        {:keys [headers request-method]} 
-       (render-response (allusers) headers request-method))
+       (render-response (request-method (allusers)) headers))
   (GET user-route 
        {:keys [headers request-method params]} 
-       (render-response (user (Integer/parseInt (:id params))) headers request-method))
+       (render-response (request-method (user (Integer/parseInt (:id params)))) headers))
   (GET carts-of-user-route
        {:keys [headers request-method params]} 
-       (render-response (carts-of-user (Integer/parseInt (:id params))) headers request-method))
+       (render-response (request-method (carts-of-user (Integer/parseInt (:id params)))) headers))
   (GET cart-route
         {:keys [headers request-method params]} 
-        (render-response (cart (Integer/parseInt (:id params))) headers request-method)) 
+        (render-response (request-method (cart (Integer/parseInt (:id params)))) headers)) 
   (route/not-found "Route Not Found"))
 
 
